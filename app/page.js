8c767b4a5e6f00e33af0ad2180c1669e6efdc3a3
sharifstack <sharifahmed.sh408@gmail.com>
2025-11-23 01:08:39 +0600

    Banner Done

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header/Index";
+import Banner from "./components/Banner/Index";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Banner />
     </div>
   );
 };
